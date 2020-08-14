de7d0e5a48da48f806a4433cc4bfb913b009cb82
Suvid Datta <suvid.datta@gmail.com>
2020-08-14 12:00:58 +0530

    overwrite : ACTIVATED

--- editor/__init__.py
+++ editor/__init__.py
@@ -1,9 +1,13 @@
 import os
-
+from time import sleep
 def getPath(path,mode):
     if mode == 'w':
         with open(path, mode='w+') as to_be_edited:
             Edit_And_overwrite(to_be_edited)
+
+    elif mode == 'ov':
+        overwrite(path)
+
     else:
         with open(path, mode='r') as read:
             Readout(read)
@@ -22,3 +26,14 @@ def Edit_And_overwrite(file):
 def Readout(file):
     for line in file:
         print(line)
+
+def overwrite(file):
+    x = input("Are you sure you want to do it?")
+    if x == "y":
+        f = open(file, 'r+')
+        f.truncate(0)
+    elif x == "n":
+        sleep(2)
+    else:
+        print("error, please answer with y or n")
+        overwrite(file)
