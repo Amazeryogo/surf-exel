ac01e7e91a55ed2d786d7ec3a35638e175b22c43
Suvid Datta <suvid.datta@gmail.com>
2020-10-14 20:14:23 +0530

    A CRACKDOWN ON WINDOWS 7 users
    We do not accept windows 7 , sorry for the inconvinience

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import editor
+import platform 
 
 from editor import root
 from editor import *
 from editor.colourx import *
-root.mainloop()
+if platform.system() == 'Windows'
+    if platform.release() == '7'
+        print('Windows 7 is not supported, please install an older version of surf-exel ')
+        root.mainloop()
+    else:
+        root.mainloop()
+else:
+    root.mainloop()
