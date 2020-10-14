34047055d7b266db066804fbe3fe13e6e4c9d210
Suvid Datta <suvid.datta@gmail.com>
2020-10-14 21:20:49 +0530

    :sweat:

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import platform
 from editor import root
 from editor import *
 from editor.colourx import *
-if platform.system() == 'Windows'
+if platform.system() == 'Windows':
     if platform.release() == '7'
         print('Windows 7 is not supported, please install an older version of surf-exel ')
         root.mainloop()
