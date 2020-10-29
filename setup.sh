6a3cda221e6831313c90ee0c527a655afb7f6e56
Suvid Datta <suvid.datta@gmail.com>
2020-10-29 19:24:46 +0530

    Set url not working , but now it  is

--- setup.sh
+++ setup.sh
@@ -9,6 +9,8 @@ pip3 install playsound
 
 git init 
 
+git remote set-url origin git@github.com:Amazeryogo/surf-exel.git
+
 git fetch --all && git checkout --force "origin/master"
 
 echo "surf-exel should work now , there is a problem if surf-exel is not working properly"
--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@ echo updating
 
 git init 
 
-
+git remote set-url origin git@github.com:Amazeryogo/surf-exel.git
 
 git fetch --all && git checkout --force "origin/master"
