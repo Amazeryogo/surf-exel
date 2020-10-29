51214fe5ae1f48abf023e7c4bee13ec74e48fe87
Suvid Datta <suvid.datta@gmail.com>
2020-10-29 19:06:51 +0530

    autoupdates it while compiling

--- setup.sh
+++ setup.sh
@@ -7,5 +7,12 @@ echo "Installing all the libraries..... \n"
 pip3 install gTTS
 pip3 install playsound
 
+git init 
+
+
+
+git fetch --all && git checkout --force "origin/master"
+
+
 echo "surf-exel should work now , there is a problem if surf-exel is not working properly"
 python3 main.py
