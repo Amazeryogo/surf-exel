8de143fbd3d39730c3b938111f719d32c87729d9
Suvid Datta <suvid.datta@gmail.com>
2020-10-29 19:35:45 +0530

    Pyinstaller is back

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ echo "Installing all the libraries..... \n"
 
 pip3 install gTTS
 pip3 install playsound
+pip3 install pyinstaller
 
 git init 
 
@@ -14,4 +15,6 @@ git remote set-url origin git@github.com:Amazeryogo/surf-exel.git
 git fetch --all && git checkout --force "origin/master"
 
 echo "surf-exel should work now , there is a problem if surf-exel is not working properly"
-python3 main.py
+pyinstaller main.py
+cd dist 
+./main
