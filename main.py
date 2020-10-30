4067e5602747a76fdebdcf233c7b50272646eb07
Suvid Datta <suvid.datta@gmail.com>
2020-10-30 18:57:30 +0530

    sorry sorry

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ import platform
 from editor import root
 from editor import *
 from editor.colourx import *
-if platform.system() == 'Windows' and platform.release() != '7':
-    print('Windows 7 sucks, please install an older version of surf-exel ')
+if platform.system() == 'Windows' and platform.release() != '10':
+    print('Your OS is not supported,please install an older version of surf-exel ')
     
