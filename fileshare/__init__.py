c790f09d8a7b2a179ea5d09441f04cf6af78dba3
Amazeryogo <suvid.datta@gmail.com>
2020-11-03 21:16:22 +0530

    {}

--- fileshare/__init__.py
+++ fileshare/__init__.py
@@ -1,36 +1,41 @@
 from socket import error
 import tkinter
-from tkinter import simpledialog 
+from tkinter import simpledialog
 import socket
 import os
 import tkinter
 from tkinter import simpledialog
+
 root = tkinter.Tk()
 import platform
+
 root.withdraw()
 
 ''' please read the README.md in the fileshare directory to learn more about this code '''
+
+
 def share():
     SEPARATOR = "<SEPARATOR>"
-    BUFFER_SIZE = 4096  
+    BUFFER_SIZE = 4096
     code = simpledialog.askstring("Code", "Enter the code of the reciver")
-    ip,port = code.split(':')
+    ip, port = code.split(':')
     s = socket.socket()
     try:
-        s.connect((ip,port))
+        s.connect((ip, port))
         # ASK FOR PASSWORD 
         file = tkinter.filedialog.askopenfilename(initialdir='', title="Open")
         with open(file, "rb") as f:
-            for i in range(1,100):
+            for i in range(1, 100):
                 bytes_read = f.read(BUFFER_SIZE)
                 if not bytes_read:
                     break
                 else:
                     s.sendall(bytes_read)
-            
-                  
+
+
     except:
-        tkinter.messagebox.showinfo("ERROR","ERROR, PLEASE TRY AGAIN LATER")
+        tkinter.messagebox.showinfo("ERROR", "ERROR, PLEASE TRY AGAIN LATER")
+
 
 def recive():
     hostname = socket.gethostname()
@@ -43,9 +48,9 @@ def recive():
         else:
             # WE WILL ADD SUPPORT FOR WINDOWS in the next commit of this file 
             tkinter.messagebox.showinfo('PLATFORM NOT SUPPORTED', "PLATFORM NOT SUPPORTED")
-            
 
     print(IP)
     print(platform.system())
-    
+
+
 recive()
