7ea6dc108f33b30c00ca9440797e68b000003a87
Amazeryogo <suvid.datta@gmail.com>
2020-11-07 14:53:34 +0530

    some more changes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import editor
 import platform 
 from editor import root
 from editor import *
-from editor.colourx import *
+from editor.settings import *
 
 if platform.system() == 'Windows':
     print('Your OS is not supported,please install an older version of surf-exel ')
--- setup.sh
+++ setup.sh
@@ -8,13 +8,9 @@ pip3 install gTTS
 pip3 install playsound
 pip3 install pyinstaller
 
-git init 
-
-git remote set-url origin git@github.com:Amazeryogo/surf-exel.git
-
-git fetch --all && git checkout --force "origin/master"
 
 echo "surf-exel should work now , there is a problem if surf-exel is not working properly"
 pyinstaller --onefile main.py 
 cd dist 
 ./main
+cd ..
