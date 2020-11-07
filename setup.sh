e0a47262ca283a5f658a9277be46b807f582a3f7
Amazeryogo <suvid.datta@gmail.com>
2020-11-07 23:31:08 +0530

    WARNING , THIS IS A TEST!

--- setup.sh
+++ setup.sh
@@ -8,9 +8,7 @@ pip3 install gTTS
 pip3 install playsound
 pip3 install pyinstaller
 
-
+x= pwd
 echo "surf-exel should work now , there is a problem if surf-exel is not working properly"
-pyinstaller --onefile main.py 
-cd dist 
+pyinstaller --onefile main.py --distpatch x
 ./main
-cd ..
